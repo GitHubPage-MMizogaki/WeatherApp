7d74ac7fd761c9ae60bcade679c92ce2f72c9694
tosee2016 <zzr1100co@gmail.com>
2016-11-13 17:30:54 +0900

    add

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -17,18 +17,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var todayLabel: UILabel!
     @IBOutlet weak var todayImage: UIImageView!
-    @IBOutlet weak var todayWeatherLabel: UILabel!
-    @IBOutlet weak var todayTemperatureLabel: UILabel!
 
     @IBOutlet weak var tomorrowLabel: UILabel!
     @IBOutlet weak var tomorrowImage: UIImageView!
-    @IBOutlet weak var tomorrowWeatherLabel: UILabel!
-    @IBOutlet weak var tomorrowTemperatureLabel: UILabel!
+
     
     @IBOutlet weak var afterTomorrowLabel: UILabel!
     @IBOutlet weak var afterTomorrowImage: UIImageView!
-    @IBOutlet weak var afterTomorrowWeatherLabel: UILabel!
-    @IBOutlet weak var afterTomorrowTemperatureLabel: UILabel!
+
     
     @IBAction func zenkokubutton(_ sender: UIButton) {
     }
@@ -63,37 +59,30 @@ class ViewController: UIViewController {
             if let forecasts = json["forecasts"].array {
                 
                 // 今日の天気
-                let todayWeather = forecasts[0]
-                
-                self.todayLabel.text = todayWeather["dateLabel"].stringValue
-                if let imgUrl = todayWeather["image"]["url"].string {
+                var box = forecasts[0]
+                self.todayLabel.text = box["dateLabel"].stringValue + "\n" + box["telop"].stringValue + "\n" + self.generateTemperatureText(box["temperature"])
+                if let imgUrl = box["image"]["url"].string {
                     self.todayImage.sd_setImage(with: URL(string: imgUrl))
                 }
-                self.todayWeatherLabel.text = todayWeather["telop"].stringValue
-                self.todayTemperatureLabel.text = self.generateTemperatureText(todayWeather["temperature"])
                 
                 // 明日の天気
-                let tomorrowWeather = forecasts[1]
+                var box1 = forecasts[1]
                 
-                self.tomorrowLabel.text = tomorrowWeather["dateLabel"].stringValue
-                if let imgUrl = tomorrowWeather["image"]["url"].string {
+                self.tomorrowLabel.text = box1["dateLabel"].stringValue + "\n" + box["telop"].stringValue + "\n" + self.generateTemperatureText(box1["temperature"])
+                if let imgUrl = box1["image"]["url"].string {
                     self.tomorrowImage.sd_setImage(with: URL(string: imgUrl))
                 }
-                self.tomorrowWeatherLabel.text = tomorrowWeather["telop"].stringValue
-                self.tomorrowTemperatureLabel.text = self.generateTemperatureText(tomorrowWeather["temperature"])
-                
+
                 
                 // 明後日の天気
                 // 気象情報の更新が入るまで明後日の天気情報が存在しない場合があるので要素数をチェックします
                 if forecasts.count >= 3 {
                     let afterTomorrowWeather = forecasts[2]
                     
-                    self.afterTomorrowLabel.text = afterTomorrowWeather["dateLabel"].stringValue
+                    self.afterTomorrowLabel.text = afterTomorrowWeather["dateLabel"].stringValue + "\n" + afterTomorrowWeather["telop"].stringValue  + "\n" + self.generateTemperatureText(afterTomorrowWeather["temperature"])
                     if let imgUrl = afterTomorrowWeather["image"]["url"].string {
                         self.afterTomorrowImage.sd_setImage(with: URL(string: imgUrl))
                     }
-                    self.afterTomorrowWeatherLabel.text = afterTomorrowWeather["telop"].stringValue
-                    self.afterTomorrowTemperatureLabel.text = self.generateTemperatureText(afterTomorrowWeather["temperature"])
                 }
                 
             }
